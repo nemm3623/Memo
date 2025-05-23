26fe7250dd091ce30a727ccc18401d6656d10a18
6zeropk <nemm3623@naver.com>
2025-05-23 13:44:08 +0900

    update/ 레포지토리 제목, 내용으로 메모 찾기

--- src/main/java/com/example/memo/MemoRepository.java
+++ src/main/java/com/example/memo/MemoRepository.java
@@ -2,5 +2,11 @@ package com.example.memo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 
-public interface MemoRepository extends JpaRepository<Memo, Long> {}
+
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findByTitleContainingOrContentContaining(String title, String content);
+    List<Memo> findByTitleContaining(String title);
+    List<Memo> findByContentContaining(String content);
+}

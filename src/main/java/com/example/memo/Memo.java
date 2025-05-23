7a2db56cd85752012b80ede9bd90b3434662b491
6zeropk <nemm3623@naver.com>
2025-05-23 13:42:31 +0900

    update/ Memo엔티티 NotNull 추가

--- src/main/java/com/example/memo/Memo.java
+++ src/main/java/com/example/memo/Memo.java
@@ -1,5 +1,6 @@
 package com.example.memo;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -12,7 +13,9 @@ public class Memo{
     @Id
     @GeneratedValue
     private Long id;
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false)
     private String content;
 
     protected Memo(){}
